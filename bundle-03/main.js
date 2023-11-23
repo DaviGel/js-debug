f0e04c0fd228953bba1e4580042a72c6164734e2
DaviGel <geloso.davide@gmail.com>
2023-11-23 16:12:52 +0100

    feat: bundle 03

--- bundle-03/main.js
+++ bundle-03/main.js
@@ -9,69 +9,73 @@
 ​
 *******************************************************************************/
 
-
 // ESERCIZIO 1 (suggerimento: ci sono 6 errori)
 const cars = [
-    {
-        manufacturer: 'Ford',
-        model: 'Fiesta',
-        type: 'diesel'
-    },
-    {
-        manufacturer: 'Audi',
-        model: 'A1',
-        type: 'benzina'
-    },
-    {
-        manufacturer: 'Volkswagen',
-        model: 'Golf',
-        type: 'Benzina'
-    },
-    {
-        manufacturer: 'Fiat',
-        model: 'Panda',
-        type: 'metano'
-    },
-    {
-        manufacturer: 'Fiat',
-        model: 'Multipla',
-        type: 'GPL'
-    },
-    {
-        manufacturer: 'Tesla',
-        model: 'Model 3',
-        type: 'elettrico'
-    },
-    {
-        manufacturer: 'Volkswagen',
-        model: 'Polo',
-        type: 'benzina'
-    },
-    {
-        manufacturer: 'Ford',
-        model: 'Kuga',
-        type: 'Diesel'
-    },
-    {
-        manufacturer: 'Seat',
-        model: 'Ibiza',
-        type: 'metano'
-    }
-    {
-        manufacturer: 'Audi',
-        model: 'R8',
-        type: 'Benzina'
-    },
+  {
+    manufacturer: 'Ford',
+    model: 'Fiesta',
+    type: 'diesel',
+  },
+  {
+    manufacturer: 'Audi',
+    model: 'A1',
+    type: 'benzina',
+  },
+  {
+    manufacturer: 'Volkswagen',
+    model: 'Golf',
+    type: 'Benzina',
+  },
+  {
+    manufacturer: 'Fiat',
+    model: 'Panda',
+    type: 'metano',
+  },
+  {
+    manufacturer: 'Fiat',
+    model: 'Multipla',
+    type: 'GPL',
+  },
+  {
+    manufacturer: 'Tesla',
+    model: 'Model 3',
+    type: 'elettrico',
+  },
+  {
+    manufacturer: 'Volkswagen',
+    model: 'Polo',
+    type: 'benzina',
+  },
+  {
+    manufacturer: 'Ford',
+    model: 'Kuga',
+    type: 'Diesel',
+  },
+  {
+    manufacturer: 'Seat',
+    model: 'Ibiza',
+    type: 'metano',
+  },
+  {
+    manufacturer: 'Audi',
+    model: 'R8',
+    type: 'Benzina',
+  },
 ];
 
-const gasolineCars = cars.filter( (auto) >= auto.type === 'benzina');
+const gasolineCars = cars.filter(
+  (cars) => cars.type.toLowerCase() === 'benzina'
+);
 
-const dieselCars = cars.filter( (auto) => {
-    auto.type === 'diesel';
+const dieselCars = cars.filter((cars) => {
+  return cars.type.toLowerCase() === 'diesel';
 });
 
-const otherCars = cars.filter( (auto) => {
-    return auto.type !== 'benzina' || auto.type !== 'diesel';
+const otherCars = cars.filter((cars) => {
+  return (
+    cars.type.toLowerCase() !== 'benzina' &&
+    cars.type.toLowerCase() !== 'diesel'
+  );
 });
 
 console.log('Auto a benzina');
@@ -84,3 +88,6 @@ console.log(dieselCars);
 
 console.log('Tutte le altre auto');
 console.log(otherCars);
+
+// Crea un array di oggetti. In gasolineCars salva le auto a benzina. In dieselCars salva le auto a diesel, in otherCars salva le altre auto. Infine stampa in console le auto a benzina, quelle a diesel e poi tutte le altre
+// Errori: manca una virgola nel penultimo oggetto, tutti gli auto vanno sostituiti con carso, nel filter la funzione freccia ha la sintassi ">=" invece di "=>", il return del filter deve essere && non || poiché non voglio né le auto diesel né le auto a benzina, siccome benzina e diesel hanno l'iniziale alle volte minuscola e alle volte maiuscola la convertiamo col metodo toLowerCase() in minuscolo. L'ultimo errore è che il filter salvato nella variabile dieselCars non aveva un return, per cui non ritornava niente
